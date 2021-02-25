57050cb1e96c64688263b8ed8b945b8e3505ecfe
jaumes5 <jaumes5@hotmail.com>
2021-02-25 21:30:57 +0100

    TIENS GAUTHIER

--- main.py
+++ main.py
@@ -8,28 +8,40 @@ def solve(input_file_name):
     problem = parse_file(input_file_name)
     # pprint(problem)
     output = create_streets(
-        problem["intersections_in"], int(problem["duration"]), problem["streets"]
+        problem["intersections_in"],
+        int(problem["duration"]),
+        problem["streets"],
+        problem["black_list"],
     )
     with open("output_" + input_file_name, "w") as output_file:
         output_file.write(output)
 
 
-def create_streets(intersections, time, streets_dict):
+def create_streets(intersections, time, streets_dict, blacklist):
     max_time = min(10, time)
-    res = str(len(intersections)) + "\n"
+    res = ""
+    inter_count = 0
     for inter in intersections.items():
-        res += inter[0] + "\n"
         if len(inter[1]) == 1:
+            res += inter[0] + "\n"
             res += "1\n" + inter[1][0] + " " + str(time) + "\n"
+            inter_count += 1
         else:
             streets = sorted_streets(inter[1], streets_dict)
             num_streets = len(inter[1])
             streets_time = [
-                i + " " + str(int(round(max_time - max_time * (acc / num_streets))))
+                i
+                + " "
+                + str(1)
                 for acc, i in enumerate(streets)
                 if round(max_time - max_time * (acc / num_streets)) != 0
+                and i not in blacklist
             ]
-            res += str(len(streets_time)) + "\n" + "\n".join(streets_time) + "\n"
+            if len(streets_time) > 0:
+                res += inter[0] + "\n"
+                res += str(len(streets_time)) + "\n" + "\n".join(streets_time) + "\n"
+                inter_count += 1
+    res = str(inter_count) + "\n" + res
     return res
 
 
