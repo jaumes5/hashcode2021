f16c2ab5d1056e426dc0ea2641555806fb2bcc71
jaumes5 <jaumes5@hotmail.com>
2021-02-25 20:03:42 +0100

    first output

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ def main():
     streets = {"rue_lo": (0,1)}
     problem = parse_file('a.txt')
     pprint(problem)
+    print(create_streets(problem["intersections_in"], int(problem["duration"])))
+
+
+def create_streets(intersections, time):
+    res = str(len(intersections)) + "\n"
+    for inter in intersections.items():
+        res += inter[0] + "\n"
+        if len(inter[1]) == 1:
+            res += "1\n" + inter[1][0] + " " + str(time) + "\n"
+        else:
+            res += (str(len(inter[1])) +
+                    "\n" +
+                    "\n".join(i + " " + str(time//2) for i in inter[1]) +
+                    "\n")
+    return res
 
 if __name__ == '__main__':
     main()
