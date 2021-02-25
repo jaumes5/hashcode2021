e9e06a751824951e98522eb2c2faa0cc327767b9
Anis <smail.a.anis@gmail.com>
2021-02-25 19:26:39 +0100

    add parser

---
+++ hash_code_parser.py
@@ -0,0 +1,37 @@
+
+from collections import defaultdict
+
+
+def parse_file(file_name):
+    with open(file_name, 'r') as my_file:
+        lines = my_file.read().splitlines()
+        first_line = lines[0].split(' ')
+        duration = first_line[0]
+        nb_intersections = int(first_line[1])
+        nb_streets = int(first_line[2])
+        nb_car = int(first_line[3])
+        intersections = defaultdict(list)
+        streets = {}
+        cars = {}
+        for line in lines[1:nb_streets+1]:
+            words = line.split(' ')
+            streets[words[2]] = {
+                'L': words[3],
+                'start': words[0],
+                'end': words[1],
+                'name': words[2]
+            }
+            intersections[words[0]].append(words[2])
+            intersections[words[1]].append(words[2])
+        for line in lines[nb_streets+1:]:
+            words = line.split(' ')
+            cars[words[0]] = words[1:]
+        return {
+            'duration': duration,
+            'nb_intersections': nb_intersections,
+            'nb_streets': nb_streets,
+            'nb_car': nb_car,
+            'intersections': intersections,
+            'streets': streets,
+            'cars': cars,
+        }
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # hello 
 
+from hash_code_parser import *
+
 def main():
     intersections = {0: [], 1: []}
     cars = {1: [], 2: []}
     streets = {"rue_lo": (0,1)}
-
+    problem = parse_file('a.txt')
+    print(problem)
 
 if __name__ == '__main__':
     main()
