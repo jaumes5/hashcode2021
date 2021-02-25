3a36933c9289ac80d96634ac9ee818ce54094871
jaumes5 <jaumes5@hotmail.com>
2021-02-25 20:49:55 +0100

    first output

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# hello 
+# hello
 
 from hash_code_parser import *
 from pprint import pprint
@@ -7,35 +7,46 @@ from pprint import pprint
 def solve(input_file_name):
     problem = parse_file(input_file_name)
     # pprint(problem)
-    output = create_streets(problem["intersections_in"], int(problem["duration"]))
-    with open('output_' + input_file_name, 'w') as output_file:
+    output = create_streets(
+        problem["intersections_in"], int(problem["duration"]), problem["streets"]
+    )
+    with open("output_" + input_file_name, "w") as output_file:
         output_file.write(output)
 
 
-def create_streets(intersections, time):
+def create_streets(intersections, time, streets_dict):
     res = str(len(intersections)) + "\n"
     for inter in intersections.items():
         res += inter[0] + "\n"
         if len(inter[1]) == 1:
             res += "1\n" + inter[1][0] + " " + str(time) + "\n"
         else:
-            res += (str(len(inter[1])) +
-                    "\n" +
-                    "\n".join(i + " " + str(1) for i in inter[1]) +
-                    "\n")
+            streets = sorted_streets(inter[1], streets_dict)
+            num_streets = len(inter[1])
+            res += (
+                str(num_streets)
+                + "\n"
+                + "\n".join(
+                    i + " " + str(num_streets - acc) for acc, i in enumerate(streets)
+                )
+                + "\n"
+            )
     return res
 
 
+def sorted_streets(streets, streets_dict):
+    return sorted(streets, key=lambda s: streets_dict[s]["nb_use"] / streets_dict[s]["L"], reverse=True)
+
+
 def main():
-    for l in 'abcdef':
-        solve(l + '.txt')
+    for l in "abcdef":
+        solve(l + ".txt")
 
 
 def test_():
-    solve('a.txt')
+    solve("a.txt")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
     # test_()
-
